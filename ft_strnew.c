52d6ed5b4def3c7e3100dcfb9f489461340bf016
Anhelina Shulha <ashulha@e1z2r8p8.42.us.org>
2017-03-08 19:54:19 -0800

    functions

---
+++ ft_strnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ashulha <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/02 12:44:13 by ashulha           #+#    #+#             */
+/*   Updated: 2017/03/02 12:44:15 by ashulha          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	char *str;
+
+	str = (char*)malloc(sizeof(char) * size + 1);
+	if (str == NULL)
+		return (NULL);
+	ft_strclr(str);
+	return (str);
+}
